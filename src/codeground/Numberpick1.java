23165ead6489e3eec3d31b1354ff636ad6896651
prausulab <prausu@gmail.com>
2016-11-20 19:13:04 +0900

    lab컴에서 올림

--- src/codeground/Numberpick1.java
+++ src/codeground/Numberpick1.java
@@ -12,7 +12,7 @@ class Numberpick1 {
 		   만약 본인의 PC 에서 테스트 할 때는, 입력값을 input.txt에 저장한 후 이 코드를 첫 부분에 사용하면,
 		   표준입력 대신 input.txt 파일로 부터 입력값을 읽어 올 수 있습니다.
 		   또한, 본인 PC에서 아래 메소드를 사용하지 않고 표준입력을 사용하여 테스트하셔도 무방합니다.
-		   단, Codeground 시스템에서 "제출하기" 할 때에는 반드시 이 메소드를 지우거나 주석(//) 처리 하셔야 합니다. */
+		   단, Codeground 시스템에서 "제출하기" 할 때에는 반드시 이 메소드를 지우거나 주석(//) 처리 하셔야 합니다.asd */
 		Scanner sc = new Scanner(new FileInputStream("sample_input1.txt"));
      
 		//Scanner sc = new Scanner(System.in);
